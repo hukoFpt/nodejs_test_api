7e646ce6f873bb292d62fd8fd558bad1002bff4f
hukoFpt <lynnie.nguyen.bns@gmail.com>
2024-09-18 14:21:41 +0700

    Refactor server.js to remove unnecessary code and update server start message

--- server.js
+++ server.js
@@ -8,16 +8,10 @@ const itemRoutes = require("./routes/itemRoutes");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware
 app.use(bodyParser.json());
 
-// Routes
 app.use("/api", itemRoutes);
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(port, () => {});
